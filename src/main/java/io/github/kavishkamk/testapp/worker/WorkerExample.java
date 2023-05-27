6f94c0bc46f8c62d8b3372ffe3eb0ad8181f50ac
kavishkamk <kavishkamadhushan727@gmail.com>
2023-05-27 18:55:31 +0530

    add worker verticle

--- src/main/java/io/github/kavishkamk/testapp/worker/WorkerExample.java
+++ src/main/java/io/github/kavishkamk/testapp/worker/WorkerExample.java
@@ -1,6 +1,7 @@
 package io.github.kavishkamk.testapp.worker;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import org.slf4j.Logger;
@@ -19,6 +20,10 @@ public class WorkerExample extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    vertx.deployVerticle(new WorkerVerticle(), new DeploymentOptions()
+      .setWorker(true)
+      .setWorkerPoolSize(1)
+      .setWorkerPoolName("my-worker-verticle-pool"));
     startPromise.complete();
     vertx.executeBlocking(event -> {
       Log.debug("Execute blocking code block");
---
+++ src/main/java/io/github/kavishkamk/testapp/worker/WorkerVerticle.java
@@ -0,0 +1,20 @@
+package io.github.kavishkamk.testapp.worker;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkerVerticle extends AbstractVerticle {
+
+  private static final Logger Log = LoggerFactory.getLogger(WorkerVerticle.class);
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    Log.debug("Deployed as worker verticle");
+    startPromise.complete();
+    Thread.sleep(500);
+    Log.debug("Blocking operation done");
+  }
+}
